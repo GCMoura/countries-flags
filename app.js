0b0724cbdfeb07c81707e3caddabc67ea3ed0e99
GCMoura <51333439+GCMoura@users.noreply.github.com>
2020-01-21 18:28:52 -0300

    21/01

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
-const root = document.querySelector(".root");
-const playBtn = document.querySelector(".play");
+const flagContainer = document.querySelector(".flag-container");
+const nameContainer = document.querySelector(".name-container");
+
+const playBtn = document.querySelector(".playBtn");
 
 getCountries();
 
@@ -12,92 +14,58 @@ async function getCountries() {
 
 function displayCountries(countries) {
     playBtn.addEventListener("click", () => {
-        root.innerHTML = "";
+        var sortNumber = [];
 
-        // for (let i = 0; i < 4; i++) {
-        //     var number = Math.floor(Math.random() * 250 + 1);
+        flagContainer.innerHTML = "";
+        nameContainer.innerHTML = "";
 
-        //     if (sortNumber.indexOf(number) == -1) {
-        //         sortNumber.push(number);
-        //     }
+        for (let i = 0; i < 4; i++) {
+            var number = Math.floor(Math.random() * 250 + 1);
 
-        //     const countryBtn = document.createElement("div");
-        //     countryBtn.classList.add("country-buttons");
+            if (sortNumber.indexOf(number) == -1) {
+                sortNumber.push(number);
+            }
 
-        //     countryBtn.innerHTML = `
-        //     <button class="country-name">${countries[number].name}</button>
-        //     `;
+            const countryBtn = document.createElement("div");
+            countryBtn.classList.add("countryBtn");
 
-        //     root.appendChild(countryBtn);
-        // }
-        createCountryButton(countries);
+            countryBtn.innerHTML = `
+            <button class="country-name-button">${countries[number].name}</button>
+            `;
 
-        // for (let i = 0; i < 1; i++) {
-        //     var numberArray = Math.floor(Math.random() * 4 + 1 - 1);
-        //     var numberFlag = sortNumber[numberArray];
-        // }
+            nameContainer.appendChild(countryBtn);
+        }
 
-        // let countryFlag = countries[numberFlag].flag;
+        for (let i = 0; i < 1; i++) {
+            var numberArray = Math.floor(Math.random() * 4 + 1 - 1);
+            var numberFlag = sortNumber[numberArray];
+        }
 
-        // const countryEl = document.createElement("div");
-        // countryEl.innerHTML = "";
+        let countryFlag = countries[numberFlag].flag;
 
-        // countryEl.innerHTML = `
-        // <div class="flag">
-        //     <img src="${countryFlag}" />
-        // </div>
-        // `;
+        const countryEl = document.createElement("div");
+        countryEl.innerHTML = "";
 
-        // root.appendChild(countryEl);
+        countryEl.innerHTML = `
+        <div class="flag">
+            <img src="${countryFlag}" />
+        </div>
+        `;
 
-        const countryButtons = document.querySelectorAll(".country-name");
+        flagContainer.appendChild(countryEl);
+
+        const countryButtons = document.querySelectorAll(".country-name-button");
 
         countryButtons.forEach(btn => {
             btn.addEventListener("click", () => {
                 if (btn.innerText === countries[numberFlag].name) {
-                    btn.style.backgroundColor = "green";
+                    btn.style.color = "green";
+                    btn.disabled = true;
                 } else {
-                    btn.style.backgroundColor = "red";
+                    btn.style.color = "red";
+                    btn.disabled = true;
                 }
             });
         });
     });
-}
-
-function createCountryButton(countries) {
-    var sortNumber = [];
-    for (let i = 0; i < 4; i++) {
-        var number = Math.floor(Math.random() * 250 + 1);
-
-        if (sortNumber.indexOf(number) == -1) {
-            sortNumber.push(number);
-        }
-
-        const countryBtn = document.createElement("div");
-        countryBtn.classList.add("country-buttons");
-
-        countryBtn.innerHTML = `
-        <button class="country-name">${countries[number].name}</button>
-        `;
-
-        root.appendChild(countryBtn);
-    }
-
-    for (let i = 0; i < 1; i++) {
-        var numberArray = Math.floor(Math.random() * 4 + 1 - 1);
-        var numberFlag = sortNumber[numberArray];
-    }
-
-    let countryFlag = countries[numberFlag].flag;
-
-    const countryEl = document.createElement("div");
-    countryEl.innerHTML = "";
-
-    countryEl.innerHTML = `
-    <div class="flag">
-        <img src="${countryFlag}" />
-    </div>
-    `;
-
-    root.appendChild(countryEl);
 }
