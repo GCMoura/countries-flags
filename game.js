d6839cda0b480786a9d363a2c220fe04e995e28b
GCMoura <51333439+GCMoura@users.noreply.github.com>
2020-01-21 22:18:16 -0300

    21/01

---
+++ game.js
@@ -0,0 +1,81 @@
+const flagContainer = document.querySelector(".flag-container");
+const nameContainer = document.querySelector(".name-container");
+
+const playBtn = document.querySelector(".playBtn");
+
+getCountries();
+
+async function getCountries() {
+    const res = await fetch("https://restcountries.eu/rest/v2/all");
+    const countries = await res.json();
+
+    displayCountries(countries);
+}
+
+function displayCountries(countries) {
+    playBtn.addEventListener("click", () => {
+        var sortNumber = [];
+
+        flagContainer.innerHTML = "";
+        nameContainer.innerHTML = "";
+
+        createCountryButton(countries, sortNumber);
+    });
+}
+
+function createCountryButton(countries, sortNumber) {
+    for (let i = 0; i < 4; i++) {
+        var number = Math.floor(Math.random() * 250 + 1);
+
+        if (sortNumber.indexOf(number) == -1) {
+            sortNumber.push(number);
+        }
+
+        const countryBtn = document.createElement("div");
+        countryBtn.classList.add("countryBtn");
+
+        countryBtn.innerHTML = `
+        <button class="country-name-button">${countries[number].name}</button>
+        `;
+
+        nameContainer.appendChild(countryBtn);
+    }
+
+    createCountryFlag(countries, sortNumber);
+}
+
+function createCountryFlag(countries, sortNumber) {
+    for (let i = 0; i < 1; i++) {
+        var numberArray = Math.floor(Math.random() * 4 + 1 - 1);
+        var numberFlag = sortNumber[numberArray];
+    }
+
+    let countryFlag = countries[numberFlag].flag;
+
+    const countryEl = document.createElement("div");
+    countryEl.classList.add("flag");
+
+    countryEl.innerHTML = `
+    <img src="${countryFlag}" />
+    `;
+
+    flagContainer.appendChild(countryEl);
+
+    modifyCountryButton(countries, numberFlag);
+}
+
+function modifyCountryButton(countries, numberFlag) {
+    const countryButtons = document.querySelectorAll(".country-name-button");
+
+    countryButtons.forEach(btn => {
+        btn.addEventListener("click", () => {
+            if (btn.innerText === countries[numberFlag].name) {
+                btn.style.color = "green";
+                btn.disabled = true;
+            } else {
+                btn.style.color = "red";
+                btn.disabled = true;
+            }
+        });
+    });
+}
